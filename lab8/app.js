4251ef93237cfcd827c695139722d2fe63de6ef3
EC2 Default User <ec2-user@ip-172-31-37-86.us-east-2.compute.internal>
2020-04-06 05:06:24 +0000

    final

---
+++ lab8/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// you can optionally use handlebars
+app.set("view engine", "ejs");
+app.use(express.static("public")); //access images, css, js
+
+// enable use of json
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.get("/", function(req, res){
+    res.render("index");
+} );
+
+app.post("/", function(req, res){
+    res.render("index");
+} );
+
+// running server
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+    console.log("Express server is running...");
+});
+
